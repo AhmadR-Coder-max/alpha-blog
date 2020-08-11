d335168fa93a81f203f800ec333f68c791bdd902
Ahmad Rizwan <upworka786@gmail.com>
2020-08-11 10:16:02 +0500

    Complete new action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,28 @@
 class ArticlesController < ApplicationController
-  layout false
+
+  def index
+    @articles = Article.all
+  end
 
   def show
     # byebug
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
 end
